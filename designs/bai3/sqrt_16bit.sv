580d2c02a614396bd7b6243e7f30c2ba5dd05c9b
mysticlong <103811505+mysticlong@users.noreply.github.com>
2022-11-19 14:34:13 +0700

    division 100%

--- designs/bai3/sqrt_16bit.sv
+++ designs/bai3/sqrt_16bit.sv
@@ -1,65 +1,27 @@
-module sqrt_16bit #(parameter NBITS = 8, PRECISION = 10)
-                (input logic [NBITS-1:0] A,
-                 input logic clock, reset, oReady, iValid,
-                 output logic iReady, oValid,
-                 output logic [NBITS-1:0]result);
-    
-    enum logic [1:0] {INIT, SET, CALC, SEND} state; 
-    logic [NBITS+NBITS-1:0] A_aux, Y, P;
-    logic [4:0] m;
-                           
-    always_ff @(posedge clock)begin
-        if(reset)begin
-            A_aux <= '0;
-            result <= '0;
-            m <= NBITS-1;
-            state <= INIT;
-            iReady <= 0;
-            oValid <= 0;
-        end
-         
-        else begin
-            case(state)
-                INIT: begin
-                        iReady <= 1;
-                        oValid <= 0;
-                        A_aux <= A<<<PRECISION;
-                        Y <= '0;
-                        P <= '0;
-                        m <= NBITS-1;
-                        state <= SET;       
-                end
-                
-                SET: begin
-                       if(m < NBITS)begin
-                         Y <= ((P<<<(m+1)) + (1 <<< (m+m)));
-                         state <= CALC;
-                       end
-                       else begin
-                         oValid <= 1;
-                         state <= SEND;
-                       end
-                end
-                CALC: begin
-                    if(Y < A_aux || Y == A_aux)begin
-                        A_aux <= A_aux - Y;
-                        P <= P + (1 <<< m);
-                        result[m] <= 1;
-                    end  
-                    else result[m] <= 0;
-                        m <= m-1;
-                        state <= SET;
-                end
-                
-                SEND: begin
-                    if(oReady)begin
-                        result <= result >>>(PRECISION-1);
-                        oValid <= 0;
-                        state <= INIT;
-                    end
-                end
-            endcase
-        end
-    end
-                          
-endmodule:sqrt_16bit
+`include "src/library/peasant_multi_nxn.sv"
+`include "src/library/division_n_digit.sv"
+module sqrt_16bit#(parameter n=16) (data0_i,data1_i,rst_i,clk_i,fl_o,yint_o,ydec_o);
+input logic[n-1:0] data0_i,data1_i;
+input logic rst_i,clk_i;
+output logic [2*n:0] yint_o;
+output logic [n-1:0] ydec_o;
+output logic fl_o;
+logic [2*n:0] x;
+logic [3*n+1:0] x_o,y_d;
+logic [2*n-1:0] y_mul0,y_mul1;
+logic fl_mul0,fl_mul1,fl_mul,fl_main,rst_mul0,rst_mul1,rst_mul,rst_d;
+//fl_main
+	assign fl_main=(y_d[2*n+1:16]==x_o[2*n+1:16])?1:0;
+	dff_n_data#(1,0) Fl_o(fl_main,rst_i,clk_i,fl_o);	//flag out
+//x^2+y^2=x
+	peasant_multi_nxn#(n) Y_mul0(data0_i,data0_i,rst_i,clk_i,rst_mul0,fl_mul0,y_mul0);
+	peasant_multi_nxn#(n) Y_mul1(data1_i,data1_i,rst_i,clk_i,rst_mul1,fl_mul1,y_mul1);
+	assign fl_mul=fl_mul0&fl_mul1;
+    //x
+	DffSync_n_data#(2*n+1,0) X({0,y_mul0}+{0,y_mul1},x,fl_mul,rst_i,clk_i,x);//33 bit 
+//sqrt
+	division_n_digit#(2*n+1,n) Y_d(x,x_o,rst_i,clk_i,rst_d,fl_d,y_d);//y_d 49bit
+	DffSync_n_data#(3*n+1,10) X_o(x_o,(x_o+y_d)>>1,fl_mul,rst_i,rst_d,x_o);//x_o 49bit
+	//yint_o ydec_o
+	assign yint_o=x_o[2*n:16],ydec_o=x_o[15:0];
+endmodule :sqrt_16bit
--- library/division_n_digit.sv
+++ library/division_n_digit.sv
@@ -1,18 +1,18 @@
 `include "src/library/DffSync_n.sv"
 `include "src/library/DffSync_n_val.sv"
-`include "src/library/dff_n_data.sv"
-module division_n_digit#(parameter n=32,digit=16)(data0_i,data1_i,digit_i,rst_i,clk_i,rst_o,fl_end,y_into,D,i,fl_in,b_io);
-input logic [n-1:0] data0_i,data1_i;
-input logic [digit-1:0] digit_i ;
+//`include "src/library/dff_n_data.sv"
+module division_n_digit#(parameter n=32,digit=16)(data0_i,data1_i,rst_i,clk_i,rst_o,fl_end,y_o);
+input logic [n-1:0] data0_i;
+input logic [n-1+digit:0] data1_i;
 input logic rst_i,clk_i;
-output logic rst_o,fl_end,fl_in;
-output logic [n-1:0] y_into;
-output logic [n+2*digit-1:0] D,b_io;
-output logic [4:0] i;
-logic clk_o,fl_main,rst;
-logic [n+2*digit-1:0] a,a_i,data_o,b,b_i;
-assign a[n-1+digit:digit]=data0_i,a[digit-1:0]='0,a[n+2*digit-1:n+digit]=0;
-assign b[n-1+digit:digit]=data1_i,b[digit-1:0]=digit_i,b[n+2*digit-1:n+digit]=0;
+output logic rst_o,fl_end;
+output logic [n-1+digit:0] y_o;
+ logic [n+3*digit-1:0] b_i;
+logic [5:0] i;
+logic clk_o,fl_main,rst,fl_in;
+logic [n+3*digit-1:0] a,a_i,data_o,b;
+assign a[n-1+2*digit:2*digit]=data0_i,a[2*digit-1:0]='0,a[n+3*digit-1:n+2*digit]=0;
+assign b[n-1+digit:0]=data1_i,b[n+3*digit-1:n+digit]=0;
 //dong bo xung clock
 	mux2to1_n#(1) Clk_o(0,clk_i,rst_i,clk_o);
 //flag out
@@ -21,18 +21,16 @@ assign b[n-1+digit:digit]=data1_i,b[digit-1:0]=digit_i,b[n+2*digit-1:n+digit]=0;
 	//rst out
 	dff_n#(1) Rst(fl_end,clk_i,rst);assign rst_o=rst|~fl_end;
 //i
-	DffSync_n_val#(5,31) I(i,i-1,~fl_main,rst_i,clk_o,i);
-//dong bo ngo vao 
+	DffSync_n_val#(6,47) I(i,i-1,~fl_main,rst_i,clk_o,i);
+//divison
 	//b_i
-	dff_n#(2*digit+n) B_i(b,rst_i,b_i);
-	assign 	fl_in=(data_o>=(b_i<<i))?1:0; assign b_io=b_i<<i;
-	//a_i
-	mux2to1_n#(2*digit+n) A_i(data_o,data_o-(b_i<<i),fl_in,a_i);
-    DffSync_n#(2*digit+n) Data_o(a,a_i,rst_i,clk_i&~fl_end,data_o);
-    //y_int
-     mux2to1_n#(1) Y_int(0,1,fl_in,y_into[i]);
-    //y_dec
-    assign D=a_i;
+	dff_n#(3*digit+n) B_i(b,rst_i,b_i);
+	assign 	fl_in=(data_o>=(b_i<<i))?1:0;
+	//tim so du
+	mux2to1_n#(3*digit+n) A_i(data_o,data_o-(b_i<<i),fl_in,a_i);
+    DffSync_n#(3*digit+n) Data_o(a,a_i,rst_i,clk_i&~fl_end,data_o);
+    //phan nguyen
+     mux2to1_n#(1) Y_o(0,1,fl_in,y_o[i]);
 endmodule :division_n_digit
 
 	
