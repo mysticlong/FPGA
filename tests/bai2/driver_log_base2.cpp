9718180571cfd6b6dee93884a0c6401daedbf6ad
mysticlong <103811505+mysticlong@users.noreply.github.com>
2022-11-27 13:28:03 +0700

    Delete tests directory

--- tests/bai2/driver_log_base2.cpp
+++
@@ -1,8 +0,0 @@
-#define MAX_SIM 1000000
-
-void set_random(Vtop *dut, vluint64_t sim_unit) {
-   	 dut->data0_i=rand()%65535;
-  //q	 dut->index_i=14;
-  	// dut->data1_i=rand()%65535;  
-    dut->rst_i=(sim_unit>4&&rand()%120!=0);
-}
