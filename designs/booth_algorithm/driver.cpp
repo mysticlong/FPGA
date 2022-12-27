13c4e0cb83d7ea986963cfc3880f6d3d4ae871b0
mysticlong <103811505+mysticlong@users.noreply.github.com>
2022-12-27 11:43:33 +0700

    update2

---
+++ designs/booth_algorithm/driver.cpp
@@ -0,0 +1,10 @@
+#define MAX_SIM 1000000
+int a;
+void set_random(Vtop *dut, vluint64_t sim_unit) {
+			dut->data0_i=rand()%255;//data0_i luon phai la so nguyen duong
+			a=rand()%255;
+			if(rand()%2) a=-rand()%255;
+			dut->data1_i=a;//co the thay bang a bang 1 so nguyen am cu the vd -15,bam may tinh de test
+            dut->rst_i=(sim_unit>4&&rand()%100!=0);
+    
+}
