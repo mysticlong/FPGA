9bb3913976c28ca1beec1e2ab272d06378d16a7c
mysticlong <103811505+mysticlong@users.noreply.github.com>
2023-01-02 22:34:16 +0700

    update md5

--- designs/MD5/driver.cpp
+++ designs/MD5/driver.cpp
@@ -1,7 +1,8 @@
 #define MAX_SIM 1000000
 
 void set_random(Vtop *dut, vluint64_t sim_unit) { 
-int arr[15]={1416127865,543257189,543450484,1701997929,1852404596,1768128512,0,0,0,0,0,0,0,0,0};
+//int arr[15]={1416127865,543257189,543450484,1701997929,1852404596,1768128512,0,0,0,0,0,0,0,0,0};
+int arr[15]={rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand(),rand()};
 int *ptr=arr;
 		for(int i=0;i<15;i++){
 			dut->M_i[i]=*ptr; 
