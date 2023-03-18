adf323c18ca7820af46b5e0e3d1efbcf6d56ae22
mysticlong <git@github.com>
2023-03-18 18:37:53 +0700

    mining bitcoin 92%

--- designs/SHA_256/driver.cpp
+++ designs/SHA_256/driver.cpp
@@ -1,11 +1,121 @@
-#define MAX_SIM 1000000
-
+/*#define MAX_SIM 1000000
+//sha-256
 
 void set_random(Vtop *dut, vluint64_t sim_unit) {
         			for(int i=0;i<16;i++){
-        			if(i==0) dut->m_i[i]=1937768448; 
-        			else if(i==15) dut->m_i[i]=8;
+        			if(i==0) dut->m_i[i]=rand(); 
+        			else if(i==15) dut->m_i[i]=16;
         			else dut->m_i[i]=0;	
         				}
         	            dut->rst_i=(sim_unit>4&&rand()%1000!=0);
 }
+/*/
+/*
+//UARTs
+#include <cstdlib> // for rand() function
+#define MAX_SIM 1000000
+
+int w = 0, r = 1;
+int* ptr_w = &w;
+int* ptr_r = &r;
+void set_random(Vtop* dut, vluint64_t sim_unit) {
+ if((sim_unit%60000)<20000){
+ 	*ptr_w =0;
+    *ptr_r =1;
+ 	
+ }
+    else if((sim_unit % 434 == 0) && (sim_unit > 436)){
+     
+        if (*ptr_w == 0) {
+            *ptr_r = 0;
+        } else if (*ptr_w == 9) {
+            *ptr_r = 1;
+        } else {
+            *ptr_r = rand() % 2;
+        }
+        *ptr_w = (*ptr_w + 1) % 10;
+    }
+	dut->clk_rd_i=sim_unit%2;
+	dut->clk_wr_i=sim_unit%2;
+    dut->Rx_i = r;
+    dut->ena_wr_i = (!(sim_unit%20000)) ? rand()%2 : dut->ena_wr_i;
+    dut->wr_i = rand() % 256;
+ //   dut->rst_i = (sim_unit % 1000 == 0) ? 0 : 1;
+}
+/*
+//Async_FIFO
+#define MAX_SIM 10000
+int w = 0, r = 1;
+int *ptr_w = &w;
+int *ptr_r = &r;
+
+void set_random(Vtop *dut, vluint64_t sim_unit) {
+    if (!(sim_unit%150)) {
+        *ptr_r =!r;
+    } else {
+        *ptr_r = *ptr_r;
+    }
+	*ptr_r=(!(sim_unit%15))?rand()%2:*ptr_r;
+//	*ptr_w=(!(sim_unit%15))?rand()%2:*ptr_w;
+    dut->ena_wr = r;
+    dut->ena_rd = !r;
+    dut->clk_wr =sim_unit%2;
+    dut->clk_rd = !(sim_unit % 8) ? 1 : 0;
+    dut->wr_i = rand() % 256;
+   dut->rst_i = !((sim_unit - 4) % 500) ? 0 : 1;
+}
+/*/
+//mining
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIM 100000000
+
+int counter = 0;
+int value = 1;
+int position = 0;
+//char arr[81] = {"eb94f2c7d51e989940129ab5f49ffff001d3ae24b1822515151651561516516515545465ghjh25gh"};
+char hex_arr[]={"01000000c39aa0fa65b6c0f6bdae75dd5fe2b934d155235c01f0b6c1b55c664fe98ee8d33af57b65c7f5d74df2b944195d266450dc2309cc190e2dbc00000000f6eaa79f06f42942085a5d5b00000001"}; // chuỗi kí	 tự cần chuyển sang hex
+char hex[3]; // biến lưu giá trị hex tạm thời
+
+// chuyển từng bit theo chuẩn UART 10bit
+void set_random(Vtop* dut, vluint64_t sim_unit) {
+    if (sim_unit < 20000 || position > 79) {
+        value = 1;
+        if ((dut->fl_end) == 0) position = 0;
+    } else if (sim_unit % 434 == 0 && sim_unit > 434 * 2) {
+        if (counter == 0) {
+            value = 0; // bit start UART
+        } else if (counter == 9) {
+            value = 1; // bit stop UART
+            position++;
+        } else {
+            int bit_index =8-counter; // vị trí bit cần gửi
+            if (position >= 80) {
+                value = 1; // đã gửi hết dữ liệu
+            } else {
+                // chuyển kí tự thành hex và gán bit tương ứng vào value
+              //  sprintf(hex, "%02x", arr[79 - position]);
+      				  strncpy(hex,&hex_arr[158-position*2], 2);
+                int hex_val = hex[bit_index / 4] - (hex[bit_index / 4] >= 'a' ? 'a' - 10 : '0'); // lấy giá trị hex từ kí tự hex tương ứng
+                value = (hex_val >> (3-bit_index % 4)) & 1; // lấy bit tương ứng từ giá trị hex
+            }
+        }
+        counter = (counter + 1) % 10;
+    }
+    dut->Rx_i = value;
+    dut->rst_i = (sim_unit % 1000 == 0) ? 0 : 1;
+}
+/*
+#define MAX_SIM 5000000
+
+int counter = 0;
+int value = 1;
+int position = 0;
+
+void set_random(Vtop* dut, vluint64_t sim_unit) {
+
+	
+	dut->rst_i = (sim_unit % 1000 == 0) ? 0 : 1;
+}
+*/
--- designs/SHA_256/mining.sv
+++ designs/SHA_256/mining.sv
@@ -6,26 +6,26 @@ module mining(Rx_i,rst_i,clk_i,Tx_o,fl_end,nonce_o);
 input	logic Rx_i,rst_i,clk_i;
 output	logic Tx_o,fl_end;
 output logic[31:0] nonce_o;
-parameter n=8,extend=20;
+parameter n=8,extend=4;
 //module uart
 	logic EnaWrU,clkWrU,clkRdU,FlFullU,rstSyncU,clkSyncU;
-    logic round;
+    logic roundU;
 	logic[n-1:0] WrU,RdU;
  	uart#(n,5,32) UART(
- 		Rx_i,
- 		EnaWrU,
- 		WrU,
- 		clkWrU,
- 		clkRdU,
- 		clk_i,
- 		Tx_o,
- 		RdU,
- 		round,
- 		FlFullU,
+ 		Rx_i,		//xung Rx
+ 		EnaWrU,		//=1 cho phep ghi
+ 		WrU,		//data ghi vao buffer	
+ 		clkWrU,		//clk write
+ 		clkRdU,		//clk read	
+ 		clk_i,		//clk noi
+ 		Tx_o,		//xung TX
+ 		RdU,		//doc data tu buffer
+ 		roundU,		//=0 :trong qua trinh nhan data,=1 qua trinh gui data
+ 		FlFullU,	//co bao full buffer
  		rstSyncU,   //rst bat dau
- 		clkSyncU);
+ 		clkSyncU);	//clk dong bo
  	assign clkRdU=clk_i;
- 	assign EnaWrU=~fl_main&empty;
+ 	assign EnaWrU=~fl_main&(empty|empty_o);
  	assign clkWrU=clk_i;
  	assign WrU[7:0]=hash_o_shift[7:0];
 //buffer mo rong 128byte
@@ -35,7 +35,7 @@ parameter n=8,extend=20;
 	dff_n#(1) ClkSyncU_d(clkSyncU,clk_i,clkSyncU_d);
 	AsynFIFO_n_m#(8,7,128) BlockHeader(
 		RdU,			//data write
-		FlFullU&~round, //enable write
+		FlFullU&~roundU, //enable write
 		clk_wrBL,		//clk_write
 		0,		//enable read ,cho phep doc khi tim ra nonce,khong doc data du nguyen
 		clk_i,			//clk_read
@@ -54,9 +54,9 @@ parameter n=8,extend=20;
 	     end
 	end	
 	//tao reset sha-256
-	logic round_d,rst_main,fl_end_main;
-	dff_n#(1)	Round_d(round,clk_i,round_d);
-	assign rst_main=~round|round_d;		
+	logic roundU_d,rst_main,fl_end_main;
+	dff_n#(1)	Round_d(roundU,clk_i,roundU_d);
+	assign rst_main=~roundU|roundU_d;		
 	logic[255:0] hash_o_main;	
 	sha_256#(32,16,0) Sha_main(w0[0:15],hash_o_main,rst_main,clk_i,fl_end_main,hash_o_main);
 //sha-256 second
@@ -72,40 +72,41 @@ parameter n=8,extend=20;
 	decoder#(extend) Decoder(rst_dec&rst_second,clk_i,data_dec);
 	// nonce
 	logic[31:0] nonce;
-	logic	clk_nonce;
+	logic	clk_nonce,clk_nonce_o;
 	logic [extend-1:0] rst_second_o;
 	assign rst_second_o=~data_dec[0];
-	DffSync_n#(32) Nonce(w0[2]&0,nonce+1,rst_main,clk_nonce&fl_end_main&fl_main_d,nonce);
-	assign 	nonce_o=nonce-extend;
+	assign clk_nonce_o=clk_nonce&fl_end_main&fl_main_d;
+	DffSync_n#(32) Nonce(w0[2]&0,nonce+1,rst_main,clk_nonce_o,nonce);
+	assign 	nonce_o=nonce+~extend+1;
 	//w1
 	logic[31:0] w_second[0:15];
 	assign w_second[0:2]=w0[16:18];
 	assign w_second[4]=32'h80000000;//gia tri mac dinh khi khong co chuoi ki tu
 	assign w_second[5:14]=w0[21:30];
-//	assign w_second[3]=32'h00000a77;
-	assign w_second[3]=nonce;
+	assign w_second[3]=(fl_main|fl_main_d)?nonce:nonce_o;
 	assign w_second[15]=w0[31]&0|32'h00000280;;
 	logic[255:0] hash_o_second[0:extend-1];
 	//mo rong bo sha-256
-	sha_256#(32,16,1) Second0(w_second,hash_o_main,rst_second&rst_second_o[0],clk_i|~fl_main,fl_end_second[0],hash_o_second[0]);
+	logic clk_Sha_second=clk_i;
+	sha_256#(32,16,1) Second0(w_second,hash_o_main,rst_main&rst_second&rst_second_o[0],clk_Sha_second,fl_end_second[0],hash_o_second[0]);
 		genvar s;
 		generate
 		  for (s =1; s < extend; s++) begin : SHA_INST
-		    sha_256#(32,16,1) SecondI (w_second,hash_o_main,rst_second_o[s],clk_i | ~fl_main,fl_end_second[s],hash_o_second[s]);
+		    sha_256#(32,16,1) SecondI (w_second,hash_o_main,rst_main&rst_second_o[s],clk_Sha_second,fl_end_second[s],hash_o_second[s]);
 		  end
 		endgenerate
 //flag end
-	logic fl_main;
+	logic fl_main;//=0 khi tim ra nonce
 	logic[255:0] hash_o;
 	logic[255:0] difficult,difficult_shift;
-	assign difficult[255:224]=w0[2]<<8,difficult[223:0]=0;
-	assign difficult_shift=difficult>>20;//w0[2][28:24];
+	assign difficult[255:224]=w0[18]<<8,difficult[223:0]=0;
+	assign difficult_shift=difficult>>(w0[18][31:24]);
 	always_comb begin:ok
 		clk_nonce=0;
 		for(int a=0;a<extend;a++) 
 		 begin
 			clk_nonce|=~data_dec[1][a];
-			if(hash_o_second[a]<difficult_shift) 
+			if(hash_o_second[a]<difficult_shift)
 			 begin
 				hash_o=hash_o_second[a];
 				fl_main=0;
@@ -122,13 +123,14 @@ parameter n=8,extend=20;
 	DffSync_n_data#(1,1) Rst_shift(fl_main_d,fl_main,fl_main_d,rst_main,clk_i,fl_main_d);
 	logic[255:0] hash_o_shift;
 	DffSync_n#(256) H_oS(hash_o,hash_o_shift>>8,fl_main|~fl_main_d,clkSyncU,hash_o_shift);
-	logic empty;
+	logic empty,empty_o;
 	assign empty=(hash_o_shift==0)?0:1;
+	dff_n#(1) Empty_o(empty,clkSyncU,empty_o);
 //not use
 	logic notuse,notuse_o;
 	assign notuse=fl_endBL|fl_fullBL|clk_SyncBL;
 	DffSync_n#(1) NotUse(notuse,notuse_o,rst_i,clk_i,notuse_o);
 //fl_end
-	assign fl_end=round|fl_main;
+	dff_n_data#(1,1) Fl_end(~fl_end,rstSyncU,~roundU,fl_end);
 endmodule:mining
 	
